0de9c7c4e936c277710b0fe580acc3a655ef965f
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-18 12:07:48 -0600

    Adding Carousel pt.1

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,15 +1,25 @@
+<script>
+import CarouselApp from '@/components/information/CarouselApp.vue'
+
+export default {
+  components: {
+    CarouselApp
+  }
+}
+</script>
+
 <template>
   <h1>This is an home page</h1>
+  <div id="carrousel">
+    <CarouselApp/>
+  </div>
+
+  <div id="more-info">
+    <div id="local-info">
+
+    </div>
+    <div id="hot-products">
+
+    </div>
+  </div>
 </template>
-
-<script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-// export default {
-//   name: 'HomeView',
-//   components: {
-//     HelloWorld
-//   }
-// }
-</script>
