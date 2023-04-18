2c8dfa8c9f36df1af1d4fb2e655fdce787e679a2
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-18 14:36:50 -0600

    Added some components
    Added components that show the history and information of the location

---
+++ website/src/components/information/StoreHistoryApp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="title">
+        <h1>Historia de la tienda</h1>
+    </div>
+    <div id="history">
+        <p>Pastelillos Carrizal fue creado por Leo, que desafortunadamente se quedó desempleado debido a la pandemia. Pero gracias a su gran determinación de mantener su amor por su comida, Leo empezó a experimentar con nuevos platillos y sabores desde su cocina. empezó a ofrecer sus creaciones a su familia y amistades, los cuales estuvieron encantados por su talento.</p>
+        <p>Aun así, con los desafíos que presento la pandemia, Leo nunca se rindió en abrir su sueño de abrir su propio local. El empezó a hacer entregas de sus productos a los hogares de varias personas, usando redes sociales para poder esparcir la palabra de sus creaciones culinarias. Dicha palabra fue esparcida rápidamente, y antes de que Leo se diera cuenta, Leo creo una gran cantidad de seguidores leales los cuales esperaron con ansias su próxima entrega.</p>
+        <p>Encantado en el gran éxito de su negocio de entregas, Leo empezó a investigar múltiples métodos con los cuales el podría abrir su negocio. Después de varios meses de trabajo duro, el por fin encontró su manera y logro por fin abrir Pastelillos Carrizal.</p>
+        <p>Hoy, Pastelillos Carrizal es un lugar bastante exitoso que continua en entregar productos de calidad con sabores únicos. La pasión de Leo por la comida y su compromiso a la calidad son evidentes en cada platillo que sale de su cocina, y su trabajo duro y perseverancia dieron buenos frutos. El negocio se volvió un lugar amado por la comunidad, y la historia de Leo continúa inspirando a las demás personas a siempre seguir sus sueños y perseguir sus pasiones, sin importar con qué tipo de retos se encuentren.</p>
+    </div>
+</template>
+
+<style scoped>
+#title {
+    color: #ffa753;
+    text-align: center;
+    border-bottom: 2px solid #ffa753; 
+}
+
+#history {
+    margin-top: 20px;
+    text-align: justify;
+}
+</style>
---
+++ website/src/components/information/StoreInfoApp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="title">
+        <h1>Sobre la tienda</h1>
+    </div>
+    <div id="info">
+        <p>Bienvenido a Pastelillos Carrizal, donde nuestra pasión por la comida y el servicio se ve reflejado en los platillos que hacemos. Nuestro local ofrece un hermoso diseño en las paredes para el que lo visite, pero también ofrecemos un rápido servicio expresó para hacerle llegar nuestros productos.</p>
+        <p>Nuestro menú fue realizado con bastante cuidado para lograr abarcar una gran variedad de platillos que le agradaran incluyendo a las personas más quisquillosas.</p>
+        <p>En Pastelillos Carrizal, creemos que comer debería ser una experiencia de saborear y de disfrutar. Desde el momento que obtienes nuestros productos, podrás sentir esa experiencia única de nuestros platillos.</p>
+    </div>
+</template>
+
+<style scoped>
+#title {
+    color: #ffa753;
+    text-align: center;
+    border-bottom: 2px solid #ffa753; 
+}
+
+#info {
+    margin-top: 20px;
+    text-align: justify;
+}
+</style>
