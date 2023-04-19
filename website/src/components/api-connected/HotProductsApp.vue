ff5741bcc60f07d84b18838dd254f35ea48db7fa
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-19 12:27:19 -0600

    Created 2 product components
    Created a component which will show a list of all the products available, and also one that will show the hot-items

---
+++ website/src/components/api-connected/HotProductsApp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="title">
+        <h1>Mejores productos</h1>
+    </div>
+</template>
+
+<style scoped>
+#title {
+    color: #ffa753;
+    text-align: center;
+    border-bottom: 2px solid #ffa753;
+    margin-bottom: 20px; 
+}
+</style>
