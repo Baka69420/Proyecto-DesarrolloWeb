d4ff39b8f25ba13cfc9aec9bb70347fdd7e60393
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-19 15:54:41 -0600

    Added video
    Added a video to the home screen.

--- website/src/components/information/ProductQualityApp.vue
+++ website/src/components/information/ProductQualityApp.vue
@@ -2,7 +2,14 @@
     <div id="title">
         <h1>Deliciosos productos</h1>
     </div>
+
+    <video id="quality-product" width="500" height="500" controls autoplay muted>
+        <source src="@/assets/videos/product-quality.mp4" type="video/mp4">
+    </video>
+
 </template>
+
+
 
 <style scoped>
 #title {
@@ -10,5 +17,12 @@
     text-align: center;
     border-bottom: 2px solid #ffa753;
     margin-bottom: 20px; 
+}
+
+#quality-product {
+    display: block;
+    max-width: 80%;
+    height: 20%;
+    margin: 0 auto;
 }
 </style>
