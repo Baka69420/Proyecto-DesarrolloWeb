14b8721b247249ca650735f9820035b29f395d3c
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-19 13:26:23 -0600

    Update ProductsListApp.vue
    Finished the Product List component, it will now obtain data from the corresponding API and display it (later on, the api will be edited to show a complete list of the products)

--- website/src/components/api-connected/ProductsListApp.vue
+++ website/src/components/api-connected/ProductsListApp.vue
@@ -1,6 +1,35 @@
+<script>
+    export default {
+        data(){
+            return {
+                products: []
+            }
+        },
+        methods: {
+            obtenerProductos(){
+                fetch("https://baka69420.github.io/Proyecto-DesarrolloWeb/api/products.json").then(response => {
+                    if(response.ok) return response.json();
+                }).then(json => {
+                    this.products = json;
+                })
+            }
+        },
+        beforeMount(){
+            this.obtenerProductos();
+        }
+    }
+</script>
+
 <template>
     <div id="title">
         <h1>Todos los productos</h1>
+    </div>
+    <div id="products">
+        <div v-for="product in products" class="product" :key="product['id']">
+            <img :src="product['img-url']" :alt="'Photo of' + product['name']">
+            <h5>{{ product['name'] }}</h5>
+            <p>Costo: {{ product['cost'] }}</p>
+        </div>
     </div>
 </template>
 
@@ -10,5 +39,60 @@
     text-align: center;
     border-bottom: 2px solid #ffa753;
     margin-bottom: 20px; 
+}
+
+#products {
+    width: 100%;
+    height: auto;
+    display: flex;
+    gap: 5px;
+    flex-wrap: wrap;
+}
+
+#products .product {
+    flex-grow: 1;
+    flex-shrink: 0;
+    height: fit-content;
+    word-wrap: normal;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    border: black solid 1px;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: #fecb70;
+}
+
+#products .product h5, #products .product p {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    word-wrap: normal;
+    margin-bottom: 0;
+}
+
+#products .product img {
+    height: 150px;
+    width: 150px;
+    display: block;
+    border-radius: 100%;
+    margin: 0 auto;
+    margin-bottom: 20px;
+}
+
+@media only screen and (max-width: 575px) {
+    #products .product {
+        flex-basis: 100%;    
+    }
+}
+
+@media only screen and (min-width: 575px) and (max-width: 1000px) {
+    #products .product {
+        flex-basis: 45%;    
+    }
+}
+
+@media only screen and (min-width: 1000px) {
+    #products .product {
+        flex-basis: 30%;    
+    }
 }
 </style>
