0d39fead79e7fcc54c492d3aa98af0827a87ed43
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-19 02:47:07 -0600

    Update ContactView.vue
    Added simple styling design
    
    Included the components that should be on the contact information

--- website/src/views/ContactView.vue
+++ website/src/views/ContactView.vue
@@ -1,18 +1,58 @@
+<script>
+import ContactInfoApp from '@/components/information/ContactInfoApp.vue'
+import EmailFormApp from '@/components/api-connected/EmailFormApp.vue'
+import MapApp from '@/components/information/MapApp.vue'
+
+export default {
+  components: {
+    ContactInfoApp,
+    EmailFormApp,
+    MapApp
+  }
+}
+</script>
+
 <template>
-    <div class="contact">
-      <h1>This is an contact page</h1>
+    <div id="contact-data">
+      <div id="contact-info">
+        <div id="contact-socials">
+          <ContactInfoApp/>
+        </div>
+        <div id="email-form">
+          <EmailFormApp/>
+        </div>
+      </div>
+      <div id="map-location">
+        <MapApp/>
+      </div>
     </div>
-  </template>
+</template>
+
+<style>
+#contact-data{
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+#contact-info {
+  display: flex;
+  gap: 20px;
+}
+
+@media only screen and (max-width: 1000px) {
+  #contact-info {
+    flex-direction: column;
+  }
+}
+
+@media only screen and (min-width: 1000px) {
+  #contact-info {
+    flex-direction: row;
+  }
+}
+</style>
   
-  <script>
-  // @ is an alias to /src
-  // import HelloWorld from '@/components/HelloWorld.vue'
-  
-  // export default {
-  //   name: 'HomeView',
-  //   components: {
-  //     HelloWorld
-  //   }
-  // }
-  </script>
+
   
