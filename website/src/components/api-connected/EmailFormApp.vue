fb29679232ca2c197348a10b164f68bd2e84c0a5
Rafael Alvarado <eduaz2002@gmail.com>
2023-04-19 02:42:12 -0600

    Create EmailFormApp.vue
    Added a form that can be used for email communication.
    
    At the moment it is only a placeholder, as it has no logic

---
+++ website/src/components/api-connected/EmailFormApp.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="title">
+        <h1>Formulario de Contacto por Email</h1>
+    </div>
+    <form>
+        <div id="name">
+            <label for="Nombre">Nombre:</label>
+            <input type="text">
+        </div>
+        <div id="email">
+            <label for="Email">Email:</label>
+            <input type="email">
+        </div>
+        <div id="subject">
+            <label for="Asunto">Asunto:</label>
+            <input type="text">
+        </div>
+        <div id="message">
+            <label for="Mensaje">Mensaje:</label>
+            <input type="text">
+        </div>
+        <div id="buttons">
+            <button type="submit">Enviar</button>
+            <button type="reset">Cancelar</button>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+#title {
+    color: #ffa753;
+    text-align: center;
+    border-bottom: 2px solid #ffa753;
+    margin-bottom: 20px; 
+}
+
+form {
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    margin: 0 auto;
+}
+
+form * {
+    margin-left: auto;
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+}
+
+form #buttons {
+    width: 100%;
+}
+
+form #buttons button {
+    background-color: #ffa753;
+    border-radius: 10px;
+    width: 50%;
+    height: 40px;
+}
+</style>
